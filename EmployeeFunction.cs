8393e83c828eda51bcf24189a2af44adec467eda
phetanshi <padmasekhar.pottepalem@outlook.com>
2023-05-30 13:43:27 +0530

    Add project files.

---
+++ EmployeeFunction.cs
@@ -0,0 +1,56 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using PS.AZ.EmpFunctionApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PS.AZ.EmpFunctionApp
+{
+    public static class EmployeeFunction
+    {
+        [FunctionName("GetEmployee")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            int.TryParse(req.Query["empId"],out int empId);
+
+            string responseMessage = $"We get the employee id :{empId}";
+            return new OkObjectResult(responseMessage);
+        }
+
+        [FunctionName("SaveEmployee")]
+        public static async Task<IActionResult> SaveEmployee(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            int.TryParse(req.Query["empId"], out int empId);
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+            List<Employee> employees = JsonConvert.DeserializeObject<List<Employee>>(requestBody);
+
+            Employee emp = employees.FirstOrDefault(x => x.EmployeeId == empId);
+
+            string responseMessage = "-No Name-";
+
+            if (emp != null)
+            {
+                responseMessage = $"{emp.FirstName} {emp.LastName}";
+            }
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.AZ.EmpFunctionApp.Models
+{
+    public class Employee
+    {
+        public int? EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
