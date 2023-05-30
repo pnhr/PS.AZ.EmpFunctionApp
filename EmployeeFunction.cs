e8edd5fd2acf20e66673cdb5597d8ab1549c44d3
phetanshi <padmasekhar.pottepalem@outlook.com>
2023-05-30 13:56:48 +0530

    Test Chagnes

--- EmployeeFunction.cs
+++ EmployeeFunction.cs
@@ -24,7 +24,7 @@ namespace PS.AZ.EmpFunctionApp
 
             int.TryParse(req.Query["empId"],out int empId);
 
-            string responseMessage = $"We get the employee id :{empId}";
+            string responseMessage = $"We got the employee id \"{empId}\" from you!";
             return new OkObjectResult(responseMessage);
         }
 
