0a08ed7a3a5183bfa12f80dd767538f3c6df2cb4
phetanshi <padmasekhar.pottepalem@outlook.com>
2023-06-01 15:10:15 +0530

    Added Few columns

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -17,6 +17,15 @@ namespace PS.AZ.EmpFunctionApp.Models
         public string? LastName { get; set; }
         [Column("SALARY")]
         public decimal? Salary { get; set; }
+
+        [Column("IMG_PATH")]
+        public decimal? ImagePath { get; set; }
+
+        [Column("GENDER")]
+        public char? Gender { get; set; }
+
+
+
         [Column("IS_ACTIVE")]
         public bool IsActive { get; set; } = true;
     }
